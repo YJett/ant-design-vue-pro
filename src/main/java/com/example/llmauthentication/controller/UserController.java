37bd0093f374a68251e322d94c554b19f8b72a93
little li <lichanglong666@qq.com>
2024-03-16 16:00:09 +0800

    authorize

--- src/main/java/com/example/llmauthentication/controller/UserController.java
+++ src/main/java/com/example/llmauthentication/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.llmauthentication.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +27,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @GetMapping("/page")
     public PageResult<User> getUserPage(
             @RequestParam(value = "pageNum", defaultValue = "1") int currentPage,
@@ -41,7 +43,7 @@ public class UserController {
     }
 
 
-
+    @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @PostMapping("/upload")
     public Result<Integer> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         if (!file.isEmpty()) {
@@ -52,29 +54,35 @@ public class UserController {
         }
     }
 
+    @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @GetMapping("/{externalUserId}")
     public Result<User> getUserByExternalUserId(@PathVariable String externalUserId) {
         User user = userService.findByExternalUserId(externalUserId);
         return Result.success(user);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @PostMapping("/")
     public Result<Void> createUser(@RequestBody User user) {
         userService.insertUser(user);
         return Result.success();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @PutMapping("/{studentId}/access")
     public Result<Void> updateCanAccess(@PathVariable String studentId, @RequestParam("canAccess") int canAccess) {
         userService.updateCanAccess(studentId, canAccess);
         return Result.success();
     }
 
+    /*
     @DeleteMapping("/{externalUserId}")
     public ResponseEntity<Void> deleteUser(@PathVariable String externalUserId) {
         userService.deleteUser(externalUserId);
         return ResponseEntity.ok().build();
     }
+
+     */
 
 
 }
--- src/main/java/com/example/llmauthentication/demos/web/BasicController.java
+++ src/main/java/com/example/llmauthentication/demos/web/BasicController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
  */
-@RequestMapping("/test")
+@RequestMapping("")
 @Controller
 public class BasicController {
 
