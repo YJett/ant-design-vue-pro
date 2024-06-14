91790f66336aaad16bc1969fc7a33c906492480d
Arthur-Boyle <ryouwei99@163.com>
2024-06-14 23:16:27 +0800

    学生素养

---
+++ src/main/java/com/example/llmauthentication/service/StuCharacterScoreService.java
@@ -0,0 +1,13 @@
+package com.example.llmauthentication.service;
+
+import com.example.llmauthentication.pojo.StuCharacterScore;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author arthur
+* @description 针对表【stu_character_score】的数据库操作Service
+* @createDate 2024-06-14 13:13:01
+*/
+public interface StuCharacterScoreService extends IService<StuCharacterScore> {
+
+}
