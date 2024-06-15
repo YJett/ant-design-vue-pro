e2340da10011dd0553c86c1f83feda6dd3a25b6a
Arthur-Boyle <ryouwei99@163.com>
2024-06-15 08:04:22 +0800

    学生素养fix

---
+++ src/main/java/com/example/llmauthentication/mapper/StuCharacterScoreMapper.java
@@ -0,0 +1,18 @@
+package com.example.llmauthentication.mapper;
+
+import com.example.llmauthentication.pojo.StuCharacterScore;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author arthur
+* @description 针对表【stu_character_score】的数据库操作Mapper
+* @createDate 2024-06-14 13:13:01
+* @Entity com.example.llmauthentication.pojo.StuCharacterScore
+*/
+public interface StuCharacterScoreMapper extends BaseMapper<StuCharacterScore> {
+
+}
+
+
+
+
