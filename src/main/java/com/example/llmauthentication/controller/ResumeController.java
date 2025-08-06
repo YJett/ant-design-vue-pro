a3db74c73c8e4a99f51033c10dee249d5efe4723
Arthur-Boyle <ryouwei99@163.com>
2025-08-06 10:52:21 +0800

    加跨域注解@CrossOrigin

--- src/main/java/com/example/llmauthentication/controller/ResumeController.java
+++ src/main/java/com/example/llmauthentication/controller/ResumeController.java
@@ -6,10 +6,7 @@ import com.example.llmauthentication.pojo.WordInfo;
 import com.example.llmauthentication.service.StudentInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +26,7 @@ import java.util.Map;
  */
 @Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/resume")
 public class ResumeController {
     @Autowired
